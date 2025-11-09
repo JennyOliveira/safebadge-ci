2bc0ea857d2abaa6ee98d1ed310c76c623761763
JennyOliveira <164917091+JennyOliveira@users.noreply.github.com>
2025-11-09 19:27:50 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,19 @@
+variable "region" {
+  default = "us-east-1"
+}
 
+variable "environment" {
+  default = "dev"
+}
+
+variable "s3_bucket_name" {
+  default = "safebadge-bucket-example-123456"
+}
+
+variable "ami_id" {
+  default = "ami-0c02fb55956c7d316"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
