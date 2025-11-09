049bc340c91f6bc588fc3ae58a3296c028d8c405
JennyOliveira <164917091+JennyOliveira@users.noreply.github.com>
2025-11-09 19:27:31 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,71 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
 
+provider "aws" {
+  region = var.region
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_s3_bucket" "safe_bucket" {
+  bucket = var.s3_bucket_name
+  acl    = "private"
+
+  tags = {
+    Name = "safe-badge-bucket"
+    Env  = var.environment
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "safebadge-web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "safebadge-web-sg"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  security_groups = [aws_security_group.web_sg.name]
+
+  tags = {
+    Name = "safebadge-web"
+    Env  = var.environment
+  }
+}
