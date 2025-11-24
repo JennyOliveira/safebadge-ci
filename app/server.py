9d2e937556843a537921a602a8add1126b1365aa
JennyOliveira <164917091+JennyOliveira@users.noreply.github.com>
2025-11-24 16:59:14 -0300

    Create server.py

---
+++ app/server.py
@@ -0,0 +1,10 @@
+# app/server.py
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "SafeBadge up!", "status": "ok"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
