ade0f35750c41cb4ccd5e0633f4c06496d6c48c6
JennyOliveira <164917091+JennyOliveira@users.noreply.github.com>
2025-11-24 17:01:31 -0300

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-# variáveis
-IMAGE="$1"            # ex: ghcr.io/USER/safebadge:tag  (passado pelo workflow)
+IMAGE="$1"
 CONTAINER_NAME="${CONTAINER_NAME:-safebadge}"
 PORT="${PORT:-8080}"
 
-echo "Deploying image: $IMAGE"
-
-# pull da imagem
-docker pull "$IMAGE"
-
-# stop & remove container antigo se existir
+echo "Recebi imagem: $IMAGE"
+echo "Parando container anterior (se existir)..."
 if docker ps -a --format '{{.Names}}' | grep -Eq "^${CONTAINER_NAME}\$"; then
-  echo "Stopping and removing previous container..."
   docker rm -f "${CONTAINER_NAME}" || true
 fi
 
-# run novo container
+echo "Fazendo pull da imagem..."
+docker pull "$IMAGE"
+
+echo "Iniciando novo container..."
 docker run -d --name "${CONTAINER_NAME}" -p ${PORT}:8080 --restart always "$IMAGE"
 
-echo "Deployed ${IMAGE} into container ${CONTAINER_NAME} on port ${PORT}"
+echo "Deploy finalizado. Container: ${CONTAINER_NAME} Escutando porta ${PORT}"
